f9b22ebe129dfcbfc1cabb0ffcbb1125bbb6bf70
moltenmuffins <evan.tkchong@gmail.com>
2020-01-26 14:18:18 +0800

    Added multiprocessing

--- python/main.py
+++ python/main.py
@@ -1,27 +1,56 @@
 import os
-import pandas
+import pandas as pd
+import multiprocessing as mp
 from tqdm import tqdm
 from utils import check_availability, unpickle_wordlist
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 pickle_path = os.path.join(dir_path, os.pardir, 'data', 'word_list.pickle')
-avaliable_names_path = os.path.join(dir_path, os.pardir, 'data', 'available_usernames.txt')
+avaliable_names_path = os.path.join(dir_path, os.pardir, 'data', 'available_usernames.csv')
+
+def worker(word, queue):
+    '''
+    Receives a word and checks the availability
+    of that word as a username
+    '''
+    if check_availability(word):
+        result = (word, True)
+    else:
+        result = (word, False)
+    queue.put(result)
+    return result
+
+def writer(queue):
+    with open(avaliable_names_path, "w") as fd:
+        while True:
+            message = queue.get()
+            if message == 'kill':
+                tqdm.write('Writer process killed')
+                break
+            else:
+                word, result = message
+                fd.write('{},{}\n'.format(word, result))
+                fd.flush()
 
 if __name__ == "__main__":
     # Unpickle our preprocessed list of words
     word_list = unpickle_wordlist(pickle_path)
+    num_process = mp.cpu_count()//2
+    print('Running {} processes'.format(num_process))
 
-    # Iterate through the list of words,
-    # while saving the still available usernames
-    # to a list
-    available_usernames = []
+    manager = mp.Manager()
+    queue = manager.Queue()
+    pool = mp.Pool(2)
+    file_writer = pool.apply_async(writer, (queue,))
 
-    for word in tqdm(word_list):
-        if check_availability(word):
-            tqdm.write('{} is available'.format(word))
-            available_usernames.append(word)
+    jobs = []
+    for word in word_list:
+        job = pool.apply_async(worker, (word, queue))
+        jobs.append(job)
 
-    # We then save the list of available usernames 
-    # to a text file
-    with open(avaliable_names_path, "w") as fd:
-        fd.writelines(available_usernames)
+    for job in tqdm(jobs):
+        job.get()
+
+    queue.put('kill')
+    pool.close()
+    pool.join
